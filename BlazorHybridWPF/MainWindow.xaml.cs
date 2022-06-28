288023ee06cfb73fbecc13b9f3864717930831a4
tecxx <tecxx@rrs.at>
2022-06-28 22:12:39 +0200

    add code

---
+++ BlazorHybridWPF/MainWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Radzen;
+using System.Windows;
+
+namespace BlazorHybridWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            var serviceCollection = new ServiceCollection();
+            serviceCollection.AddWpfBlazorWebView();
+            serviceCollection.AddScoped<DialogService>();
+            serviceCollection.AddScoped<NotificationService>();
+            serviceCollection.AddScoped<TooltipService>();
+            serviceCollection.AddScoped<ContextMenuService>();
+            Resources.Add("services", serviceCollection.BuildServiceProvider());
+        }
+    }
+}
